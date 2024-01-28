537e6b44b9c0f93afc49c011d25ec872727f4415
Josh Shavers <shaversj@gmail.com>
2024-01-28 09:03:38 -0600

    Nav Bar Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import Nav from "./Nav.jsx";
 
 function App() {
-
   return (
     <>
+      <div className={"flex min-h-screen items-center justify-center bg-black"}>
+        <div className={"min-h-[667px] min-w-[375px] max-w-screen-sm bg-red-600"}>
+          {/*Nav*/}
+          <Nav />
 
+          <main>
+            {/*  Search*/}
+            <div></div>
+
+            {/*Trending*/}
+            <section></section>
+
+            {/*Recommended for you*/}
+            <section></section>
+          </main>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      "sm": "375px",
-      "lg": "1440px"
+      sm: "375px",
+      lg: "1440px",
     },
     extend: {
       colors: {
         "bright-red": "#fc4747",
-        "very-dark-blue": "#10141E",
-        "desaturated-dark-blue": "#5A698F",
-        "dark-blue": "#161D2F",
+        "dark-blue": "#10141E",
+        "greyish-blue": "#5A698F",
+        "semi-dark-blue": "#161D2F",
       },
       fontFamily: {
-        "Outfit": ["Outfit", "sans-serif"]
-      }
+        Outfit: ["Outfit", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
