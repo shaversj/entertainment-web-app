8fca8310d3b84734f6554e2af17c409b68fbfebc
Josh Shavers <shaversj@gmail.com>
2024-01-28 09:34:18 -0600

    Search Bar Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Nav from "./Nav.jsx";
+import Search from "./Search.jsx";
 
 function App() {
   return (
     <>
       <div className={"flex min-h-screen items-center justify-center bg-black"}>
-        <div className={"min-h-[667px] min-w-[375px] max-w-screen-sm bg-red-600"}>
-          {/*Nav*/}
-          <Nav />
-
+        <div className={"min-h-[667px] min-w-[375px] max-w-screen-sm bg-dark-blue font-Outfit"}>
           <main>
+            {/*Nav*/}
+            <Nav />
+
             {/*  Search*/}
-            <div></div>
+            <Search />
 
             {/*Trending*/}
             <section></section>
