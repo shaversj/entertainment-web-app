046e2e685b4cff1247aef812b18e0c10294c8ea4
Josh Shavers <shaversj@gmail.com>
2024-02-04 09:43:06 -0600

    Make SectionList component dynamic

--- src/SectionList.jsx
+++ src/SectionList.jsx
@@ -1,19 +1,25 @@
 import Card from "./Card.jsx";
 
 const SectionList = ({ title, data, handleBookmark }) => {
+  const filteredList = data.filter((item) => !item.isTrending);
   return (
     <div className={"space-y-[16px]"}>
       <h1 className={"text-[20px] font-extralight text-white"}>{title}</h1>
-      <Card
-        title={data[5].title}
-        thumbnail={data[5].thumbnail}
-        year={data[5].year}
-        category={data[5].category}
-        rating={data[5].rating}
-        isBookmarked={data[5].isBookmarked}
-        isTrending={data[5].isTrending}
-        handleBookmark={handleBookmark}
-      />
+      <div className={"grid grid-cols-2 gap-x-4"}>
+        {filteredList &&
+          filteredList.map((item) => (
+            <Card
+              title={item.title}
+              thumbnail={item.thumbnail}
+              year={item.year}
+              category={item.category}
+              rating={item.rating}
+              isBookmarked={item.isBookmarked}
+              isTrending={item.isTrending}
+              handleBookmark={handleBookmark}
+            />
+          ))}
+      </div>
     </div>
   );
 };
